a01c7c76cb7a2ea35e72b846d4f5c346109ab29f
Indra Bayu <indra.b@skyshi.com>
2018-11-27 10:29:59 +0700

    instantiate board in main activity

--- app/src/main/java/com/example/indrabayu/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/indrabayu/myapplication/view/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.indrabayu.myapplication.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.indrabayu.myapplication.R
+import com.example.indrabayu.myapplication.model.Board
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val board = Board()
     }
 }
