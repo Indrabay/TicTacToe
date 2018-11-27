b27c755a7de937ac0364c1cfd3639388d49b2b5d
Indra Bayu <indra.b@skyshi.com>
2018-11-27 11:30:42 +0700

    create view_model

--- app/src/main/java/com/example/indrabayu/myapplication/view_model/TicTacToeViewModel.java
+++ app/src/main/java/com/example/indrabayu/myapplication/view_model/TicTacToeViewModel.java
@@ -1,11 +1,11 @@
-package com.example.indrabayu.myapplication.presenter;
+package com.example.indrabayu.myapplication.view_model;
 
 import com.example.indrabayu.myapplication.model.Board;
 import com.example.indrabayu.myapplication.model.Cell;
 import com.example.indrabayu.myapplication.model.Player;
 import com.example.indrabayu.myapplication.view.MainActivity;
 
-public class OthelloPresenter {
+public class TicTacToeViewModel {
     // Manggil View
     public MainActivity activity;
 
